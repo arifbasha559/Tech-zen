ea192492072cd5c12e2c5e165d511fd1f63bd918
Arif Basha <arifbasha559@gmail.com>
2025-02-23 15:26:36 +0530

    refactor: clean up Blog component, enhance pagination buttons, and remove unused code

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,12 @@
 import { useContext, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+// import { Link, useLocation } from "react-router-dom";
 import "../App.css";
 import BlogContext from "../context/BlogContext";
 import Card from "./Card";
 import Loader from "./Loader";
 
 const Blog = () => {
-  const location = useLocation();
+  // const location = useLocation();
   const blog = useContext(BlogContext);
   const [stories, setStories] = useState([]);
   const [page, setPage] = useState(1);
@@ -41,11 +41,14 @@ const Blog = () => {
 
   return (
     <>
-      <div className={`w-full px-4 sm:px-6 lg:px-8 ${blog.theme}`}> {/* Apply theme */}
-        <h1 className={`font-bruco w-full lg:text-8xl md:text-7xl text-5xl font-black lg:leading-snug border-b-2 mb-8 text-center ${blog.colors.border}`}>
+      <div className={`w-full px-4 sm:px-6 lg:px-8 ${blog.theme}`}>
+        {" "}
+        {/* Apply theme */}
+        <h1
+          className={`font-bruco w-full lg:text-8xl md:text-7xl text-5xl font-black lg:leading-snug border-b-2 mb-8 text-center ${blog.colors.border}`}
+        >
           TECH ZEN
         </h1>
-
         <div className="flex w-full">
           <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mx-auto justify-center my-4">
             {loading ? (
@@ -71,35 +74,111 @@ const Blog = () => {
             )}
           </div>
         </div>
-
-        <div className="flex items-center justify-center space-x-2 my-4">
-          {page > 1 && (
-            <button
-              className="flex items-center px-3 py-2 rounded-lg bg-gray-800 hover:bg-gray-700"
-              onClick={() => handlePageChange(page - 1)}
-            >
-              <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
-              Previous
-            </button>
-          )}
-          {page > 1 && <button className="px-3 py-2 rounded-lg bg-gray-800">{page - 1}</button>}
-          <button className="px-3 py-2 rounded-lg bg-blue-600">{page}</button>
-          {page + 1 <= totalPages && <button className="px-3 py-2 rounded-lg bg-gray-800" onClick={() => handlePageChange(page + 1)}>{page + 1}</button>}
-          {page + 2 <= totalPages && <button className="px-3 py-2 rounded-lg bg-gray-800" onClick={() => handlePageChange(page + 2)}>{page + 2}</button>}
-          {page + 3 <= totalPages && <button className="px-3 py-2 rounded-lg bg-gray-800" onClick={() => handlePageChange(page + 3)}>{page + 3}</button>}
-          {page < totalPages && (
-            <>
-              <button className="px-3 py-2 rounded-lg bg-gray-800" onClick={() => handlePageChange(totalPages)}>{totalPages}</button>
+        {loading ? null : (
+          <div className="flex items-center justify-center space-x-2 py-20 max-w-[80vh] mx-auto">
+            {page > 1 && (
               <button
-                className="flex items-center px-3 py-2 rounded-lg bg-gray-800 hover:bg-gray-700"
+                className={`flex items-center px-4 py-2 rounded-lg ${
+                  blog.colors.bg !== "bg-black"
+                    ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                    : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                } `}
+                onClick={() => handlePageChange(page - 1)}
+              >
+                <svg
+                  className="w-4 h-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 19l-7-7 7-7"
+                  ></path>
+                </svg>
+                
+              </button>
+            )}
+            {page > 1 && (
+              <button
+                className={`px-4 py-2 rounded-lg ${
+                  blog.colors.bg !== "bg-black"
+                    ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                    : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                }`}
+              >
+                {page - 1}
+              </button>
+            )}
+            <button className="px-4 py-2 rounded-lg bg-blue-600">{page}</button>
+            {page + 1 <= totalPages && (
+              <button
+                className={`px-4 py-2 rounded-lg ${
+                  blog.colors.bg !== "bg-black"
+                    ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                    : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                }`}
                 onClick={() => handlePageChange(page + 1)}
               >
-                Next
-                <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+                {page + 1}
               </button>
-            </>
-          )}
-        </div>
+            )}
+            {page + 2 <= totalPages && (
+              <button
+                className={`px-4 py-2 rounded-lg ${
+                  blog.colors.bg !== "bg-black"
+                    ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                    : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                }`}
+                onClick={() => handlePageChange(page + 2)}
+              >
+                {page + 2}
+              </button>
+            )}
+            
+            {page < totalPages && (
+              <>
+                <button
+                  className={`px-4 py-2 rounded-lg ${
+                    blog.colors.bg !== "bg-black"
+                      ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                      : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                  }`}
+                  onClick={() => handlePageChange(totalPages)}
+                >
+                  {totalPages}
+                </button>
+                <button
+                  className={`flex items-center px-4 py-2 rounded-lg ${
+                    blog.colors.bg !== "bg-black"
+                      ? "bg-gradient-to-b from-[#F0F4FF] to-[#E6F9FF] text-gray-800"
+                      : "bg-gradient-to-b from-[#212121] to-[#212121] text-white"
+                  }`}
+                  onClick={() => handlePageChange(page + 1)}
+                >
+                  
+                  <svg
+                    className="w-4 h-6 "
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 5l7 7-7 7"
+                    ></path>
+                  </svg>
+                </button>
+              </>
+            )}
+          </div>
+        )}
       </div>
     </>
   );
